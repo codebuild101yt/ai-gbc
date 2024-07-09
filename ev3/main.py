3cdbeed7279bad0beffcdeeb8970c086e190c022
gray talbot <vicelikedisc947@gmail.com>
2024-07-09 09:24:14 -0400

    implimented subtracting data on main

--- ev3/main.py
+++ ev3/main.py
@@ -3,7 +3,11 @@ from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import Motor, UltrasonicSensor
 from pybricks.parameters import Port
 import time
+import ev3_comm
+from ev3_comm import EV3Server
 
+#Initialize server.
+server = EV3Server()
 
 # Manuel mode switch.
 manuel = True
@@ -47,6 +51,36 @@ while manuel == True:
                 motor.hold()
                 time.sleep(5)
 else:
-    #Insert AI code here.
-    print("test")
+
+    server.wait_for_connection()
+    while True:
+        data = server.receive_message()
+        if data == None:
+            data == 0
+        while us.distance() > 60:
+            print("Top Distance:", us.distance())
+            print("Bottom Distance:", us2.distance())
+            print("Moving Up...")
+            motor.run(-120)
+            
+        else:
+            print("Platform detected. Stopping...")
+            # Play a sound.
+            ev3.speaker.beep()
+            time.sleep(0.5)
+            motor.hold()
+            time.sleep(5)
+            while us2.distance() > 60:
+                print("Top Distance:", us.distance())
+                print("Bottom Distance:", us2.distance())
+                print("Moving Up...")
+                motor.run(120)
+            else:
+                # Play a sound.
+                ev3.speaker.beep()
+                time.sleep(0.1)
+                ev3.speaker.beep()
+                time.sleep(0.1)
+                motor.hold()
+                time.sleep(15-data)
 print("hello")
